03fc07bff8b47d8d4ff361f5513d6b5400d3ceb0
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-16 14:18:32 +0300

    urls

--- kimdjaa/awwards/wins/urls.py
+++ kimdjaa/awwards/wins/urls.py
@@ -1,11 +1,33 @@
-from django.urls import path
-from .import views
+# from django.urls import path
+# from .import views
 
 
-#urls
+# #urls
+# urlpatterns = [
+#     # path('',views.login, name="login"),
+#     # path('register/',views.registerPage, name="register"),
+#     path('awwards/', views.awwards, name='awwards'),
+#     # path('profile/', views.profile, name='profile'),
+# ]
+from django.urls import path,include
+from . import views
+from rest_framework import routers
+
+#API routes
+router = routers.DefaultRouter()
+router.register('profiles',views.ProfileView)
+router.register('projects',views.ProjectsView)
+router.register('projects_ratings',views.RatingsView)
+
+#awward app urls
 urlpatterns = [
-    # path('',views.login, name="login"),
-    # path('register/',views.registerPage, name="register"),
-    path('awwards/', views.awwards, name='awwards'),
-    # path('profile/', views.profile, name='profile'),
+    path('',views.home,name='home'),
+    path('profile/',views.profile,name='profile'),
+    path('profile/update/',views.update_profile,name='profileUpdate'),
+    path('post_project/',views.post_project,name='post_project'),
+    path('search/project/',views.search_project,name='search_project'),
+    path('rate_project/<int:project_id>',views.rate_project,name='rate'),
+    path('details/project/<int:project_id>',views.project_details,name='project_details'),
+    
+    path('api/',include(router.urls)),
 ]
