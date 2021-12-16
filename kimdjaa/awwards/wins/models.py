4c6454a97cd27516051df5e30214ff8b3ed324d4
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-16 12:55:46 +0300

    profile model

--- kimdjaa/awwards/wins/models.py
+++ kimdjaa/awwards/wins/models.py
@@ -18,3 +18,16 @@ class Projects(models.Model):
   
   def __str__(self):
     return self.name 
+class Profile(models.Model): 
+  user = models.OneToOneField(User,on_delete=models.CASCADE)
+  bio = models.TextField(max_length=400)
+  country = models.CharField(max_length=30)
+  phone_number = models.CharField(max_length=10)
+  picture = CloudinaryField('profile')
+  
+  def save_profile(self): 
+    '''Funtion to save a profile object'''
+    self.save()
+  
+  def __str__(self):
+    return self.user.username    
