68baa5bab18a27cf18b67e3bfa2c7b2568c0e6f1
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-16 12:54:43 +0300

    project model

--- kimdjaa/awwards/wins/models.py
+++ kimdjaa/awwards/wins/models.py
@@ -2,4 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 # Create your models here.
-
+#Projects model
+class Projects(models.Model): 
+  project_owner = models.ForeignKey(User,on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  description = models.TextField(max_length=400)
+  main_technology_used = models.CharField(max_length=30,null=True)
+  screenshot = CloudinaryField('screenshot')
+  live_link = models.URLField(max_length=100)
+  date_added = models.DateField(auto_now_add=True)
+  
+  def save_project(self): 
+    '''Function to save a project'''
+    self.save()
+  
+  def __str__(self):
+    return self.name 
